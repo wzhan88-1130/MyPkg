7c4e34a0ef2ab1243e1c24d5cbd0c9997a0e2db6
Documenter.jl <documenter@juliadocs.github.io>
2023-11-06 15:43:12 +0000

    build based on ecba679

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyPkg","category":"page"},{"location":"#MyPkg","page":"Home","title":"MyPkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyPkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyPkg]","category":"page"}]
+}
